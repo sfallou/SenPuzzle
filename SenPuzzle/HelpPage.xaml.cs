ace081f56a9b8217ddf9f05d33faa8e67c009816
sfallou <sfallou2010&hotmail.fr>
2018-03-03 17:12:27 +0100

    Version 0.7

--- SenPuzzle/HelpPage.xaml.cs
+++ SenPuzzle/HelpPage.xaml.cs
@@ -10,13 +10,28 @@ namespace SenPuzzle
         public HelpPage()
         {
             InitializeComponent();
+
+            TileView = new ContentView
+            {
+                Padding = new Thickness(1),
+
+                // Get the bitmap for each tile 
+                Content = new Image
+                {
+                    //Source = ImageSource.FromUri(new Uri(UrlPrefix + "Bitmap" + row + col + ".png"))
+                    Source = ImageSource.FromResource("SenPuzzle.Bitmaps.Bitmap.png")
+                }
+            };
+            absoluteLayout.Children.Add(TileView);
+
         }
 
         private async void OnBackPage(object sender, EventArgs e)
         {
-            await Navigation.PushModalAsync(new SenPuzzlePage());
-            //await Navigation.PopAsync();
+            //await Navigation.PushModalAsync(new SenPuzzlePage());
+            await Navigation.PopModalAsync();
 
-        }  
+        } 
+        public View TileView { private set; get; }
     }
 }
