97f3e2317f55112ca7fd160ac51d8672d9c342df
sfallou <sfallou2010&hotmail.fr>
2018-03-03 13:56:13 +0100

    Version 0.5

---
+++ SenPuzzle/HelpPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace SenPuzzle
+{
+    public partial class HelpPage : ContentPage
+    {
+        public HelpPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnBackPage(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new SenPuzzlePage());
+            //await Navigation.PopAsync();
+
+        }  
+    }
+}
